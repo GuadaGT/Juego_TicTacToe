4f1591b123e5c6c64a1afa2cc5a8d92814687a50
Guada <125298307+GuadaGT@users.noreply.github.com>
2023-04-27 19:49:11 +0200

    Add files via upload

---
+++ Proyecto 3_en_raya_GGT/3_en_raya/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace _3_en_raya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.lblJugador = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonReset.Font = new System.Drawing.Font("Vivaldi", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReset.ForeColor = System.Drawing.Color.Green;
+            this.buttonReset.Location = new System.Drawing.Point(356, 209);
+            this.buttonReset.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(105, 86);
+            this.buttonReset.TabIndex = 50;
+            this.buttonReset.Text = "Nueva \r\npartida";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn9.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn9.Location = new System.Drawing.Point(256, 235);
+            this.btn9.Margin = new System.Windows.Forms.Padding(2);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(85, 78);
+            this.btn9.TabIndex = 48;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn8.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn8.Location = new System.Drawing.Point(167, 235);
+            this.btn8.Margin = new System.Windows.Forms.Padding(2);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(85, 78);
+            this.btn8.TabIndex = 47;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn7.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn7.Location = new System.Drawing.Point(77, 235);
+            this.btn7.Margin = new System.Windows.Forms.Padding(2);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(85, 78);
+            this.btn7.TabIndex = 46;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn6.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn6.Location = new System.Drawing.Point(256, 153);
+            this.btn6.Margin = new System.Windows.Forms.Padding(2);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(85, 78);
+            this.btn6.TabIndex = 45;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn5.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn5.Location = new System.Drawing.Point(167, 153);
+            this.btn5.Margin = new System.Windows.Forms.Padding(2);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(85, 78);
+            this.btn5.TabIndex = 44;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn4.Location = new System.Drawing.Point(77, 153);
+            this.btn4.Margin = new System.Windows.Forms.Padding(2);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(85, 78);
+            this.btn4.TabIndex = 43;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn3.Location = new System.Drawing.Point(256, 72);
+            this.btn3.Margin = new System.Windows.Forms.Padding(2);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(85, 78);
+            this.btn3.TabIndex = 42;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn2.Location = new System.Drawing.Point(167, 72);
+            this.btn2.Margin = new System.Windows.Forms.Padding(2);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(85, 78);
+            this.btn2.TabIndex = 41;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn1.Location = new System.Drawing.Point(77, 72);
+            this.btn1.Margin = new System.Windows.Forms.Padding(2);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(85, 78);
+            this.btn1.TabIndex = 40;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_3_en_raya.Properties.Resources.Hojas_arriba;
+            this.pictureBox1.Location = new System.Drawing.Point(53, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(424, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::_3_en_raya.Properties.Resources.Hojas_lados;
+            this.pictureBox4.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(75, 406);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 38;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::_3_en_raya.Properties.Resources.Hojas_lados;
+            this.pictureBox3.Location = new System.Drawing.Point(475, -1);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 406);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 37;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::_3_en_raya.Properties.Resources.Hojas_arriba;
+            this.pictureBox2.Location = new System.Drawing.Point(67, 333);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(410, 60);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 36;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(370, 139);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(76, 66);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 51;
+            this.pictureBox5.TabStop = false;
+            // 
+            // lblJugador
+            // 
+            this.lblJugador.AutoSize = true;
+            this.lblJugador.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblJugador.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJugador.ForeColor = System.Drawing.Color.Green;
+            this.lblJugador.Location = new System.Drawing.Point(366, 99);
+            this.lblJugador.Name = "lblJugador";
+            this.lblJugador.Size = new System.Drawing.Size(0, 22);
+            this.lblJugador.TabIndex = 52;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(549, 392);
+            this.Controls.Add(this.lblJugador);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "3 en raya";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label lblJugador;
+    }
+}
+
---
+++ Proyecto 3_en_raya_GGT/3_en_raya/Form1.cs
@@ -0,0 +1,230 @@
+﻿using _3_en_raya.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_en_raya
+{
+    public partial class Form1 : Form
+    {
+        private enum fichas { VACIO, X, O };
+        private int turno;
+        private _3_en_raya.Form1.fichas[,] tablero;
+        private bool finPartida;
+        private Button celda;
+        private int x;
+        private int y;
+        private int[] index;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            Reset();
+            fichas Winner = Ganador();
+        }
+
+        private void Reset()
+        {
+            tablero = new fichas[3, 3];
+            lblJugador.Text = String.Empty;
+            foreach (Control x in this.Controls)
+                if (x is Button)
+                    ((Button)x).Image = null;
+            finPartida = false;
+        }
+
+        private int filaCelda()
+        {
+            int numCelda = Convert.ToInt32(celda.Name.Substring(3, 1));
+            return (numCelda - 1) / 3;
+        }
+
+        private int colCelda()
+        {
+            int numCelda = Convert.ToInt32(celda.Name.Substring(3, 1));
+            return (numCelda - 1) % 3;
+        }
+
+        private void efectuarTirada()
+        {
+            if (celda.Image == null && !finPartida)
+            {
+                if (turno % 2 == 0)
+                {
+                    celda.Image = Resources.Imagen0;
+                    tablero[filaCelda(), colCelda()] = fichas.O;
+                    lblJugador.Text = ("Jugador X");
+                    pictureBox5.Image = Properties.Resources.Imagenx;
+
+                }
+                else
+                {
+                    celda.Image = Resources.Imagenx;
+                    tablero[filaCelda(), colCelda()] = fichas.X;
+                    lblJugador.Text = ("Jugador 0");
+                    pictureBox5.Image = Properties.Resources.Imagen0;
+
+                }
+                fichas ganador = Ganador();
+                if (ganador != fichas.VACIO)
+                {
+                    lblJugador.Text = ganador.ToString();
+                    finPartida = true;
+                    pictureBox5.Image = null;
+                    lblJugador.Text = " ";
+                }
+                turno++;
+            }
+        }
+        private fichas comprobarFila(int x)
+        {
+            fichas Player = tablero[x, 0];
+            int j = 1;
+            for (; j < 3 && tablero[x, j] == Player; j++) ;
+            return (j == 3) ? Player : fichas.VACIO;
+        }
+
+        private fichas comprobarFilas()
+        {
+            int i = 0;
+            fichas Winner = fichas.VACIO;
+            for (; i < 3 && (Winner = comprobarFila(i)) == fichas.VACIO; i++) ;
+            return Winner;
+        }
+
+        private fichas comprobarCol(int y)
+        {
+            fichas Player = tablero[0, y];
+            int i = 1;
+            for (; i < 3 && tablero[i, y] == Player; i++) ;
+            return (i == 3) ? Player : fichas.VACIO;
+        }
+
+        private fichas comprobarCols()
+        {
+            int j = 0;
+            fichas Winner = fichas.VACIO;
+            for (; j < 3 && (Winner = comprobarCol(j)) == fichas.VACIO; j++) ;
+            return Winner;
+        }
+
+        private fichas comprobarDiagonales()
+        {
+            int i = 0;
+            fichas Player = tablero[0, 0];
+            for (; i < 3 && tablero[i, i] == Player; i++) ;
+            if (i == 3) return Player;
+            i = 0;
+            Player = tablero[0, 2];
+            for (; i < 3 && tablero[i, 2 - i] == Player; i++) ;
+            if (i == 3) return Player;
+            return fichas.VACIO;
+        }
+
+        private fichas Ganador()
+        {
+            fichas Winner = Winner = comprobarFilas();
+            if (Winner == fichas.VACIO);
+                Winner = comprobarCols();
+            if (Winner == fichas.VACIO)
+                Winner = comprobarDiagonales();
+
+
+            if (Winner is fichas.X)
+            {
+                MessageBox.Show("Gana el jugador X.");
+            }
+            if (Winner is fichas.O)
+            {
+                MessageBox.Show("Gana el jugador 0.");
+            }
+            else if (tablero[0, 0] != 0)
+            { 
+                MessageBox.Show("Tablas. ");
+            }
+            
+            return Winner;
+        }
+
+        private void repintar()
+        {
+            efectuarTirada();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            celda = (Button)sender;
+            repintar();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+            MessageBox.Show("¡Comienza la partida!");
+        }
+    }
+}
+    
